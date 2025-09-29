b72d397a0047648143dddfc0f4b11c5049fcbb73
kaylubr <kylebreyes092005@gmail.com>
2025-09-29 19:13:05 +0800

    Add a module for rendering the todo pages named todoPage.js

---
+++ src/pages/todoPage.js
@@ -0,0 +1,6 @@
+function todoPage(projectName, date = null, completedList = false) {
+  const content = document.querySelector('#content');
+  const pageTitle = document.createElement('h1');
+  const todosContainer = document.createElement('div');
+  const addTaskBtn = document.createElement('button');
+}
