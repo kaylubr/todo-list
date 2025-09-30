73e6fbbde3e27a0f19a3d8fe02370119b062a8c9
kaylubr <kylebreyes092005@gmail.com>
2025-09-30 18:20:44 +0800

    Add functionality for addings tasks and renders it immediately

--- src/controllers/ScreenController.js
+++ src/controllers/ScreenController.js
@@ -2,37 +2,35 @@ import { format } from "date-fns";
 import { addTodo } from "./TodoController";
 import todoPage from "../pages/todoPage";
 
+const navItems = document.querySelectorAll('.nav-item');
+const content = document.querySelector('#taskContainer');
+const addTaskDialog = document.querySelector('#addTaskDialog');
+const closeDialogBtn = document.querySelector('#addTaskDialog button:first-of-type');
+const confirmAddBtn = document.querySelector('#addTaskDialog button:last-of-type');
+const addTaskBtn = document.querySelector('#addTaskBtn');
+
+// Dialog inputs for adding todo's
+const projectName = document.querySelector('#projectsDropdown');
+const title = document.querySelector('#title');
+const description = document.querySelector('#description');
+const dueDate = document.querySelector('#dueDate');
+const priority = document.querySelector('#priority');
+
 const currentDate = format(new Date(), 'yyyy-MM-dd');
 
+// Runs when the DOM loads
 document.addEventListener('DOMContentLoaded', () => todoPage('inbox'));
 
+// Disable past days for dueDate
+dueDate.setAttribute('min', currentDate);
+
 export default function ScreenController() {
-  const navItems = document.querySelectorAll('.nav-item');
-  const content = document.querySelector('#taskContainer');
-  const addTaskDialog = document.querySelector('#addTaskDialog');
-  const closeDialogBtn = document.querySelector('#addTaskDialog button:first-of-type');
-  const confirmAddBtn = document.querySelector('#addTaskDialog button:last-of-type');
-  const addTaskBtn = document.querySelector('#addTaskBtn');
 
-  // Dialog inputs for adding todo's
-  const projectName = document.querySelector('#projectsDropdown');
-  const title = document.querySelector('#title');
-  const description = document.querySelector('#description');
-  const dueDate = document.querySelector('#dueDate');
-  const priority = document.querySelector('#priority');
-  
-  // Disable past days for dueDate
-  dueDate.setAttribute('min', currentDate);
+  handleAddTaskModal();
+  handleNavItems();
+}
 
-  // Opens modal for adding tasks
-  addTaskBtn.addEventListener('click', event => {
-    addTaskDialog.showModal();
-  });
-
-  // Close modal for adding tasks
-  closeDialogBtn.addEventListener('click', () => addTaskDialog.close());
-
-
+function handleNavItems() {
   navItems.forEach(item => {
     item.addEventListener('click', event => {
       const sectionName = event.currentTarget.id;
@@ -64,5 +62,24 @@ export default function ScreenController() {
           break; 
       }
     });
+  });
+}
+
+function handleAddTaskModal() {
+  // Opens modal for adding tasks
+  addTaskBtn.addEventListener('click', () => {
+    addTaskDialog.showModal();
+  });
+
+  // Close modal for adding tasks
+  closeDialogBtn.addEventListener('click', () => addTaskDialog.close());
+
+  confirmAddBtn.addEventListener('click', () => {
+    const formatedDate = format(dueDate.value, 'MM/dd/yyyy');
+    addTodo(projectName.value,title.value, description.value, formatedDate, priority.value);
+
+    content.textContent = '';
+
+    todoPage(projectName.value);
   });
 }
