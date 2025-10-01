c171750257346856398ada081b2f6e09f06dc3c2
kaylubr <kylebreyes092005@gmail.com>
2025-10-01 22:17:40 +0800

    Add functionality for rendering projects correctly

--- src/pages/projectList.js
+++ src/pages/projectList.js
@@ -4,10 +4,17 @@ import { capitalize } from "../includes/capitalize";
 export default function projectList() {
   const projectList = document.querySelector('#project-list');
   const projects = getAllProjects().filter(project => project.name !== 'inbox');
+  const projectListDropdown = document.querySelector('#projectsDropdown');
 
+  // Resets project tab on nav
   projectList.textContent = ''
 
+  // Resets dropdown
+  const childrenToRemove = Array.from(projectListDropdown.children).slice(1);
+  childrenToRemove.forEach(child => child.remove());;
+
   projects.forEach(project => {
+    // Project list for nav bar
     const li = document.createElement('li');
     li.setAttribute('id', project.name);
     li.classList.add('nav-item', 'project-item');
@@ -21,5 +28,11 @@ export default function projectList() {
 
     li.append(p, button);
     projectList.append(li);
+
+    // Project list for adding/editing todo's dialog
+    const option = document.createElement('option');
+    option.value = project.name;
+    option.textContent = project.name;
+    projectListDropdown.append(option)
   });
 }
